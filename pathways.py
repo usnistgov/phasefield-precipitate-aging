ab7d1dd0f4b71b89ff693767c0f0d9029e90fa0b
Trevor Keller <trevor.keller@nist.gov>
2017-02-17 20:17:50 -0500

    better phase diagrams

--- pathways.py
+++ pathways.py
@@ -5,11 +5,12 @@
 
 # Numerical libraries
 import numpy as np
+from math import floor, sqrt
 from scipy.optimize import fsolve
 from scipy.spatial import ConvexHull
 
 # Runtime / parallel libraries
-import glob, time, warnings
+import glob, time
 from itertools import chain
 from multiprocessing import Pool
 
@@ -43,28 +44,28 @@ for i in range(20):
 
 
 def computeKernelExclusive(n):
-    a = n / density # index along x-axis
-    b = n % density # index along y-axis
+    #a = n / density # index along x-axis
+    #b = n % density # index along y-axis
 
-    xnb = epsilon + 1.0*a / (density-1)
-    xcr = epsilon + 1.0*b / (density-1)
+    xnb = max(epsilon, 1.0 * (n / density) / density)
+    xcr = max(epsilon, 1.0 * (n % density) / density)
+
     xni = 1.0 - xcr - xnb
 
-    result = [0]*7
+    result = [0]*6
     
     if xni>0:
         result[0] = xcr
         result[1] = xnb
-        result[2] = xni
-        result[3] = GG(xcr, xnb)
-        result[4] = GD(xcr, xnb)
-        result[5] = GU(xcr, xnb)
-        result[6] = GL(xcr, xnb)
+        result[2] = TG(xcr, xnb)
+        result[3] = TD(xcr, xnb)
+        result[4] = TU(xcr, xnb)
+        result[5] = TL(xcr, xnb)
     
     return result
 
 
-density = 101
+density = 4000
 allCr = []
 allNb = []
 allG = []
@@ -73,20 +74,19 @@ points = []
 phases = []
 
 if __name__ == '__main__':
-    pool = Pool(2)
+    pool = Pool(6)
 
     i = 0
-    for result in pool.imap(computeKernelExclusive, range(density**2)):
-        xcr, xnb, xni, fg, fd, fu, fh = result
-        f = (fg, fd, fu, fh)
+    for result in pool.imap(computeKernelExclusive, range(density*(density+1))):
+        xcr, xnb, fg, fd, fu, fl = result
+        f = (fg, fd, fu, fl)
 
         # Accumulate (x, y, G) points for each phase
-        if (fd**2 + fu**2 + fh**2) > epsilon:
-            for n in range(len(f)):
-                allNb.append(simX(xnb, xcr))
-                allCr.append(simY(xcr))
-                allG.append(f[n])
-                allID.append(n)
+        for n in range(len(f)):
+            allNb.append(simX(xnb, xcr))
+            allCr.append(simY(xcr))
+            allG.append(f[n])
+            allID.append(n)
         i += 1
 
     pool.close()
@@ -108,7 +108,7 @@ for simplex in hull.simplices:
 
 
 
-for pair in ('collapse', 'gamma-delta', 'gamma-mu', 'gamma-laves', 'fourphase'):
+for pair in ('collapse', 'gamma-delta', 'gamma-mu', 'gamma-laves', 'delta-laves', 'fourphase'):
     # Plot phase diagram
     plt.figure(figsize=(10, 7.5)) # inches
     plt.plot(XS, YS, '-k')
@@ -125,11 +125,11 @@ for pair in ('collapse', 'gamma-delta', 'gamma-mu', 'gamma-laves', 'fourphase'):
     # Add composition pathways
     fnames = glob.glob("data/alloy625/run1/{0}/*.xy".format(pair))
     n = len(fnames)
-    for i in np.logspace(0, np.log10(n), 10, endpoint=False, dtype=int):
+    for i in (1, n-1): # np.logspace(0, np.log10(n), 10, endpoint=False, dtype=int):
         xcr, xnb = np.loadtxt(fnames[i], delimiter=',', unpack=True)
         plt.plot(simX(xnb, xcr), simY(xcr), '.-', markersize=2, linewidth=1)
     
     plt.xlim([0, 0.6])
     plt.ylim([0, rt3by2*0.6])
-    plt.savefig("diagrams/pathways_{0}.png".format(pair), dpi=600)
+    plt.savefig("diagrams/pathways_{0}.png".format(pair), dpi=400)
     plt.close()
--- raritan_run.sh
+++ raritan_run.sh
@@ -4,14 +4,14 @@
 #SBATCH --partition mml        # partition (mml or preemptible)
 #SBATCH --constraint="haswell" # Haswell
 #SBATCH -t 24:00:00            # time (hh:mm:ss or dd-hh:mm:ss)
-#SBATCH --nodes=4              # total machines
-#SBATCH --ntasks=16            # total MPI ranks
-#SBATCH --ntasks-per-core=16   # OMP threads per MPI rank
+#SBATCH --nodes=8              # total machines
+#SBATCH --ntasks=16            # total MPI ranks, 2 per machine
+#SBATCH --cpus-per-task=16     # OMP threads per MPI rank
 #SBATCH --overcommit           # unlock every available thread
 #SBATCH -J TKR4p074
 #SBATCH -o /wrk/tnk10/phase-field/alloy625/TKR4p074.log
 #SBATCH -D /wrk/tnk10/phase-field/alloy625/TKR4p074
-export OMP_NUM_THREADS=$SLURM_NTASKS_PER_CORE
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 # SLURM batch script for Inconel 625 coarsening simulation
 #
@@ -22,9 +22,8 @@ export OMP_NUM_THREADS=$SLURM_NTASKS_PER_CORE
 #        sbatch raritan_run.sh
 
 # Hardware notes: Raritan Haswells are Xeon E5-2630v3,
-# 8 cores per chip, 4 sockets per machine = 32 cores total.
-# Each Intel core is 2-way SMT, so each machine has 64 threads.
-# Recommend 4 MPI ranks per node with 16 threads per rank.
+# 8 cores per chip, 2 sockets per machine = 32 threads total.
+# Use a minimum of 1 MPI rank per socket.
 
 SRCDIR=/home/tnk10/research/projects/phase-field/Zhou718
 WRKDIR=/wrk/tnk10/phase-field/alloy625/TKR4p074
