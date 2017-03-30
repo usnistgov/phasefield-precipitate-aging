8a121cb1da72e119f6de1f18e875d850e4df160d
Trevor Keller <trevor.keller@nist.gov>
2017-03-30 14:26:22 -0400

    compute pycalphad landscape using equilibrium not calculate

--- landscapes.py
+++ landscapes.py
@@ -17,17 +17,18 @@
 from CALPHAD_energies import *
 
 from tqdm import tqdm
+from pycalphad import equilibrium
+from pycalphad import variables as v
 
-# Plot ternary free energy landscapes
+# setup global variables
+
 Titles = (r'$\gamma$', r'$\delta$', r'$\mu$', r'Laves')
-npts = 75
+npts = 76
 nfun = 4
 #levels = 100
-#levels = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0, 10, 100, 1000, 10000, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12]
-#span = (-0.05, 1.05)
-#yspan = (-0.15, 0.95)
-span = (-0.01, 1.01)
-yspan = (-0.1, 0.9)
+#levels = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0, 10, 100, 1000, 10000, 1e5, 1.0e10, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12]
+span = (-0.001, 1.001)
+yspan = (-0.001, rt3by2 + 0.001)
 x = np.linspace(span[0], span[1], npts)
 y = np.linspace(yspan[0], yspan[1], npts)
 z = np.ndarray(shape=(nfun,len(x)*len(y)), dtype=float)
@@ -49,7 +50,11 @@ for a in np.arange(0, 1, 0.1):
 p = np.zeros(len(x)*len(y))
 q = np.zeros(len(x)*len(y))
 
+
+# Plot CALPHAD free energies using extracted equations
+
 n = 0
+z.fill(0.0)
 for j in tqdm(np.nditer(y)):
     for i in np.nditer(x):
         xcr = 1.0*j / rt3by2
@@ -57,14 +62,14 @@ for j in tqdm(np.nditer(y)):
         xni = 1.0 - xcr - xnb
         p[n] = i
         q[n] = j
-        z[0][n] = GG(xcr, xnb)
-        z[1][n] = GD(xcr, xnb)
-        z[2][n] = GU(xcr, xnb)
-        z[3][n] = GL(xcr, xnb)
+        z[0][n] = GG(xcr, xnb) #c_gamma.subs({GAMMA_XCR: xcr, GAMMA_XNB: xnb, GAMMA_XNI: xni}) #GG(xcr, xnb)
+        z[1][n] = GD(xcr, xnb) #c_delta.subs({DELTA_XCR: xcr, DELTA_XNB: xnb, DELTA_XNI: xni}) #GD(xcr, xnb)
+        z[2][n] = GU(xcr, xnb) #c_mu.subs({MU_XCR: xcr, MU_XNB: xnb, MU_XNI: xni}) #GU(xcr, xnb)
+        z[3][n] = GL(xcr, xnb) #c_laves.subs({LAVES_XCR: xcr, LAVES_XNB: xnb, LAVES_XNI: xni}) #GL(xcr, xnb)
         n += 1
 
-datmin = 1e13
-datmax = 1e-13
+datmin = 1.0e10
+datmax = -1.0e10
 for n in range(len(z)):
     mymin = np.min(z[n])
     mymax = np.max(z[n])
@@ -88,9 +93,10 @@ for ax in axarr.reshape(-1):
     ax.set_ylim(yspan)
     ax.axis('off')
     for a in range(len(XG)):
-        ax.plot(XG[a], YG[a], ':w')
+        ax.plot(XG[a], YG[a], ':w', linewidth=0.5)
     ax.tricontourf(p, q, z[n]-1.01*datmin, levels, cmap=plt.cm.get_cmap('coolwarm'), norm=LogNorm())
-    ax.plot(XS, YS, ':w')
+    #ax.tricontourf(p, q, z[n], cmap=plt.cm.get_cmap('coolwarm'))
+    ax.plot(XS, YS, 'k', linewidth=0.5)
     ax.scatter(X0[n], Y0[n], color='black', s=2.5)
     n+=1
 plt.figtext(x=0.5, y=0.0625, ha='center', fontsize=8, \
@@ -105,9 +111,10 @@ for n in range(len(z)):
     plt.ylim(yspan)
     plt.axis('off')
     for a in range(len(XG)):
-        plt.plot(XG[a], YG[a], ':w')
+        plt.plot(XG[a], YG[a], ':w', linewidth=0.5)
     plt.tricontourf(p, q, z[n]-1.01*datmin, levels, cmap=plt.cm.get_cmap('coolwarm'), norm=LogNorm())
-    plt.plot(XS, YS, ':w')
+    #plt.tricontourf(p, q, z[n], cmap=plt.cm.get_cmap('coolwarm'))
+    plt.plot(XS, YS, 'k', linewidth=0.5)
     plt.scatter(X0[n], Y0[n], color='black', s=2.5)
     plt.margins(0,0)
     plt.gca().xaxis.set_major_locator(plt.NullLocator())
@@ -117,30 +124,10 @@ for n in range(len(z)):
 
 
 
-# Plot ternary free energy landscapes
-Titles = (r'$\gamma$', r'$\delta$', r'$\mu$', r'Laves')
-x = np.linspace(span[0], span[1], npts)
-y = np.linspace(yspan[0], yspan[1], npts)
-z = np.ndarray(shape=(nfun,len(x)*len(y)), dtype=float)
-
-# Triangular grid
-XG = [[]]
-YG = [[]]
-for a in np.arange(0, 1, 0.1):
-    # x1--x2: lines of constant x2=a
-    XG.append([simX(a, 0), simX(a, 1-a)])
-    YG.append([simY(0),    simY(1-a)])
-    # x2--x3: lines of constant x3=a
-    XG.append([simX(0, a), simX(1-a, a)])
-    YG.append([simY(a),    simY(a)])
-    # x1--x3: lines of constant x1=1-a
-    XG.append([simX(0, a), simX(a, 0)])
-    YG.append([simY(a),    simY(0)])
-
-p = np.zeros(len(x)*len(y))
-q = np.zeros(len(x)*len(y))
+# Plot Taylor series approximate free energy landscapes
 
 n = 0
+z.fill(0.0)
 for j in tqdm(np.nditer(y)):
     for i in np.nditer(x):
         xcr = 1.0*j / rt3by2
@@ -154,8 +141,8 @@ for j in tqdm(np.nditer(y)):
         z[3][n] = TL(xcr, xnb)
         n += 1
 
-datmin = 1e13
-datmax = 1e-13
+datmin = 1.0e10
+datmax = -1.0e10
 for n in range(len(z)):
     mymin = np.min(z[n])
     mymax = np.max(z[n])
@@ -176,9 +163,10 @@ for ax in axarr.reshape(-1):
     ax.set_ylim(yspan)
     ax.axis('off')
     for a in range(len(XG)):
-        ax.plot(XG[a], YG[a], ':w')
+        ax.plot(XG[a], YG[a], ':w', linewidth=0.5)
     ax.tricontourf(p, q, z[n]-1.01*datmin, levels, cmap=plt.cm.get_cmap('coolwarm'), norm=LogNorm())
-    ax.plot(XS, YS, ':w')
+    #ax.tricontourf(p, q, z[n], cmap=plt.cm.get_cmap('coolwarm'))
+    ax.plot(XS, YS, 'k', linewidth=0.5)
     ax.scatter(X0[n], Y0[n], color='black', s=2.5)
     n+=1
 plt.figtext(x=0.5, y=0.0625, ha='center', fontsize=8, \
@@ -193,9 +181,10 @@ for n in range(len(z)):
     plt.ylim(yspan)
     plt.axis('off')
     for a in range(len(XG)):
-        plt.plot(XG[a], YG[a], ':w')
+        plt.plot(XG[a], YG[a], ':w', linewidth=0.5)
     plt.tricontourf(p, q, z[n]-1.01*datmin, levels, cmap=plt.cm.get_cmap('coolwarm'), norm=LogNorm())
-    plt.plot(XS, YS, ':w')
+    #plt.tricontourf(p, q, z[n], cmap=plt.cm.get_cmap('coolwarm'))
+    plt.plot(XS, YS, 'k', linewidth=0.5)
     plt.scatter(X0[n], Y0[n], color='black', s=2.5)
     plt.margins(0,0)
     plt.gca().xaxis.set_major_locator(plt.NullLocator())
@@ -203,29 +192,13 @@ for n in range(len(z)):
     plt.savefig(files[n], transparent=True, dpi=400, bbox_inches='tight', pad_inches=0)
     plt.close()
 
-'''
-
-span = (-0.01, 1.01)
-yspan = (-0.1, 0.9)
-
-# Triangular grid
-XG = [[]]
-YG = [[]]
-for a in np.arange(0, 1, 0.1):
-    # x1--x2: lines of constant x2=a
-    XG.append([simX(a, 0), simX(a, 1-a)])
-    YG.append([simY(0),    simY(1-a)])
-    # x2--x3: lines of constant x3=a
-    XG.append([simX(0, a), simX(1-a, a)])
-    YG.append([simY(a),    simY(a)])
-    # x1--x3: lines of constant x1=1-a
-    XG.append([simX(0, a), simX(a, 0)])
-    YG.append([simY(a),    simY(0)])
 
 
+
+
+
+"""
 # Plot ternary free energy landscapes from CALPHAD
-Titles = (r'$\gamma$', r'$\delta$', r'$\mu$', r'Laves')
-
 XX  = [[], [], [], []]
 YY  = [[], [], [], []]
 AllG  = [[], [], [], []]
@@ -234,22 +207,25 @@ XNb = [[], [], [], []]
 G   = [[], [], [], []]
 
 
-gamma_data = calculate(tdb, elements, 'FCC_A1', T=temp, output='GM')
+fulldb = Database('Du_Cr-Nb-Ni.tdb')
+elements = ['CR', 'NB', 'NI', 'VA']
+
+gamma_data = calculate(fulldb, elements, 'FCC_A1', T=temp)
 XCr[0] = np.ravel(gamma_data.X.sel(component='CR'))
 XNb[0] = np.ravel(gamma_data.X.sel(component='NB'))
 G[0]   = np.ravel(gamma_data.GM)
 
-delta_data = calculate(tdb, elements, 'D0A_NBNI3', T=temp, output='GM')
+delta_data = calculate(fulldb, elements, 'D0A_NBNI3', T=temp)
 XCr[1] = np.ravel(delta_data.X.sel(component='CR'))
 XNb[1] = np.ravel(delta_data.X.sel(component='NB'))
 G[1]   = np.ravel(delta_data.GM)
 
-mu_data = calculate(tdb, elements, 'D85_NI7NB6', T=temp, output='GM')
+mu_data = calculate(fulldb, elements, 'D85_NI7NB6', T=temp)
 XCr[2] = np.ravel(mu_data.X.sel(component='CR'))
 XNb[2] = np.ravel(mu_data.X.sel(component='NB'))
 G[2]   = np.ravel(mu_data.GM)
 
-laves_data = calculate(tdb, elements, 'C15_LAVES', T=temp, output='GM')
+laves_data = calculate(fulldb, elements, 'C14_LAVES', T=temp)
 XCr[3] = np.ravel(laves_data.X.sel(component='CR'))
 XNb[3] = np.ravel(laves_data.X.sel(component='NB'))
 G[3]   = np.ravel(laves_data.GM)
@@ -260,27 +236,27 @@ for n in range(4):
         YY[n].append(simY(XCr[n][i]))
 
 for i in range(len(G[0])):
-    AllG[0].append(G[0][i] - TG(xcr, xnb))
+    AllG[0].append(G[0][i])
 
 for i in range(len(G[1])):
-    AllG[1].append(G[1][i] - TG(xcr, xnb))
+    AllG[1].append(G[1][i])
 
 for i in range(len(G[2])):
-    AllG[2].append(G[2][i] - TU(xcr, xnb))
+    AllG[2].append(G[2][i])
 
 for i in range(len(G[3])):
-    AllG[3].append(G[3][i] - TL(xcr, xnb))
+    AllG[3].append(G[3][i])
 
 
-#datmin = 1.0e8
-#datmax = -1.0e8
-#for n in range(len(AllG)):
-#    mymin = np.min(AllG[n])
-#    mymax = np.max(AllG[n])
-#    datmin = min(datmin, mymin)
-#    datmax = max(datmax, mymax)
-#
-#print "Data spans [%.4g, %.4g]" % (datmin, datmax)
+datmin = 1.0e10
+datmax = -1.0e10
+for n in range(len(AllG)):
+    mymin = np.min(AllG[n])
+    mymax = np.max(AllG[n])
+    datmin = min(datmin, mymin)
+    datmax = max(datmax, mymax)
+
+print "Data spans [%.4g, %.4g]" % (datmin, datmax)
 
 #levels = np.logspace(np.log2(datmin-1.0001*datmin), np.log2(datmax-1.01*datmin), num=50, base=2.0)
 #levels = np.linspace(datmin-1.01*datmin, datmax-1.01*datmin, 50)
@@ -293,13 +269,13 @@ for n in range(len(G)):
     plt.ylim(yspan)
     plt.axis('off')
     for a in range(len(XG)):
-        plt.plot(XG[a], YG[a], ':w')
+        plt.plot(XG[a], YG[a], ':w', linewidth=0.5)
     #offset = [1.01*datmin] * len(AllG[n])
-    #plt.tricontourf(XX[n], YY[n], G[n]-1.01*np.min(G[n]), levels, cmap=plt.cm.get_cmap('coolwarm'), norm=LogNorm())
+    #plt.tricontourf(XX[n], YY[n], G[n]-1.0001*datmin, levels, cmap=plt.cm.get_cmap('coolwarm'), norm=LogNorm())
     #plt.tricontourf(XX[n], YY[n], AllG[n]-offset, levels, cmap=plt.cm.get_cmap('coolwarm'))
     #plt.tricontourf(XX[n], YY[n], AllG[n], levels=levels, cmap=plt.cm.get_cmap('coolwarm'))
     plt.tricontourf(XX[n], YY[n], AllG[n], cmap=plt.cm.get_cmap('coolwarm'))
-    plt.plot(XS, YS, ':w')
+    plt.plot(XS, YS, 'k', linewidth=0.5)
     plt.scatter(X0[n], Y0[n], color='black', s=2.5)
     plt.margins(0,0)
     plt.gca().xaxis.set_major_locator(plt.NullLocator())
@@ -319,13 +295,13 @@ for ax in axarr.reshape(-1):
     ax.set_ylim(yspan)
     ax.axis('off')
     for a in range(len(XG)):
-        ax.plot(XG[a], YG[a], ':w')
+        ax.plot(XG[a], YG[a], ':w', linewidth=0.5)
     #offset = [1.01*datmin] * len(AllG[n])
-    #ax.tricontourf(XX[n], YY[n], AllG[n]-1.01*np.min(AllG[n]), levels, cmap=plt.cm.get_cmap('coolwarm'), norm=LogNorm())
+    #ax.tricontourf(XX[n], YY[n], AllG[n]-1.0001*datmin, levels, cmap=plt.cm.get_cmap('coolwarm'), norm=LogNorm())
     #ax.tricontourf(XX[n], YY[n], AllG[n]-offset, levels, cmap=plt.cm.get_cmap('coolwarm'))
     #ax.tricontourf(XX[n], YY[n], AllG[n], levels=levels, cmap=plt.cm.get_cmap('coolwarm'))
     ax.tricontourf(XX[n], YY[n], AllG[n], cmap=plt.cm.get_cmap('coolwarm'))
-    ax.plot(XS, YS, ':w')
+    ax.plot(XS, YS, 'k', linewidth=0.5)
     #ax.scatter(X0[n], Y0[n], color='black', s=2.5)
     n+=1
 
@@ -334,4 +310,88 @@ plt.figtext(x=0.5, y=0.0625, ha='center', fontsize=8, \
 f.savefig('ternary_calphad.png', dpi=400, bbox_inches='tight')
 plt.close()
 
-'''
+"""
+
+
+
+#fulldb = Database('Du_Cr-Nb-Ni.tdb')
+#elements = ['CR', 'NB', 'NI']
+
+p = np.zeros(len(x)*len(y))
+q = np.zeros(len(x)*len(y))
+
+# Plot CALPHAD free energy landscape using pycalphad
+n = 0
+z.fill(0.0)
+for j in tqdm(np.nditer(y)):
+    for i in np.nditer(x):
+        xcr = 1.0*j / rt3by2
+        xnb = 1.0*i - 0.5 * j / rt3by2
+        xsum = xcr + xnb
+        xni = 1.0 - xcr - xnb
+        p[n] = i
+        q[n] = j
+        if xsum < 1 and xcr > 0 and xnb > 0 and xni > 0:
+            g_nrg = inVm * float(equilibrium(tdb, elements, 'FCC_A1',     {T:temp, v.P:101325, v.X('CR'):xcr, v.X('NB'):xnb}, output='GM').GM)
+            z[0][n] = g_nrg
+        if xsum < 1 and xcr > 0 and xnb > 0 and xni > 0 and xnb < 0.25 and xcr < 0.75:
+            d_nrg = inVm * float(equilibrium(tdb, elements, 'D0A_NBNI3',  {T:temp, v.P:101325, v.X('CR'):xcr, v.X('NB'):xnb}, output='GM').GM)
+            z[1][n] = d_nrg
+        if xsum < 1 and xcr > 0 and xnb > 0 and xni > 0 and xnb > fr6by13:
+            m_nrg = inVm * float(equilibrium(tdb, elements, 'D85_NI7NB6', {T:temp, v.P:101325, v.X('CR'):xcr, v.X('NB'):xnb}, output='GM').GM)
+            z[2][n] = m_nrg
+        if xsum < 1 and xcr > 0 and xnb > 0 and xni > 0 and xnb < fr1by3 and xni < fr2by3:
+            l_nrg = inVm * float(equilibrium(tdb, elements, 'C14_LAVES',  {T:temp, v.P:101325, v.X('CR'):xcr, v.X('NB'):xnb}, output='GM').GM)
+            z[3][n] = l_nrg
+        n += 1
+
+datmin = 1.0e10
+datmax = -1.0e10
+for n in range(len(z)):
+    mymin = np.min(z[n])
+    mymax = np.max(z[n])
+    datmin = min(datmin, mymin)
+    datmax = max(datmax, mymax)
+
+print "Data spans [%.4g, %.4g]" % (datmin, datmax)
+
+levels = np.logspace(np.log2(datmin-1.01*datmin), np.log2(datmax-1.01*datmin), num=50, base=2.0)
+
+f, axarr = plt.subplots(nrows=2, ncols=2, sharex='col', sharey='row')
+f.suptitle("IN625 Ternary Potentials (CALPHAD)",fontsize=14)
+n=0
+for ax in axarr.reshape(-1):
+    ax.set_title(Titles[n],fontsize=10)
+    ax.axis('equal')
+    ax.set_xlim(span)
+    ax.set_ylim(yspan)
+    ax.axis('off')
+    for a in range(len(XG)):
+        ax.plot(XG[a], YG[a], ':w', linewidth=0.5)
+    ax.tricontourf(p, q, z[n]-1.01*datmin, levels, cmap=plt.cm.get_cmap('coolwarm'), norm=LogNorm())
+    #ax.tricontourf(p, q, z[n], cmap=plt.cm.get_cmap('coolwarm'))
+    ax.plot(XS, YS, 'k', linewidth=0.5)
+    ax.scatter(X0[n], Y0[n], color='black', s=2.5)
+    n+=1
+plt.figtext(x=0.5, y=0.0625, ha='center', fontsize=8, \
+            s=r'White triangles enclose Gibbs simplex, $x_{\mathrm{Cr}}+x_{\mathrm{Nb}}+x_{\mathrm{Ni}}=1$.')
+f.savefig('ternary_calphad.png', dpi=400, bbox_inches='tight')
+plt.close()
+
+files = ['diagrams/gamma_CALPHAD.png', 'diagrams/delta_CALPHAD.png', 'diagrams/mu_CALPHAD.png', 'diagrams/Laves_CALPHAD.png']
+for n in range(len(z)):
+    plt.axis('equal')
+    plt.xlim(span)
+    plt.ylim(yspan)
+    plt.axis('off')
+    for a in range(len(XG)):
+        plt.plot(XG[a], YG[a], ':w', linewidth=0.5)
+    plt.tricontourf(p, q, z[n]-1.01*datmin, levels, cmap=plt.cm.get_cmap('coolwarm'), norm=LogNorm())
+    #plt.tricontourf(p, q, z[n], cmap=plt.cm.get_cmap('coolwarm'))
+    plt.plot(XS, YS, 'k', linewidth=0.5)
+    plt.scatter(X0[n], Y0[n], color='black', s=2.5)
+    plt.margins(0,0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.savefig(files[n], transparent=True, dpi=400, bbox_inches='tight', pad_inches=0)
+    plt.close()
