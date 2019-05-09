5ad2c8d2d4ef2e01c29fbe9d7859623e8cc4d784
Trevor Keller <trevor.keller@nist.gov>
2019-05-09 19:15:22 -0400

    working example with an argument

--- src/pyCtest.c
+++ src/pyCtest.c
@@ -17,11 +17,16 @@ int main()
     PyObject *pyplotname    = NULL,
              *pyplotmodule  = NULL,
              *pltfigurename = NULL,
-             *pyplot_figure = NULL,
-             *py_empty_args = NULL,
-             *fig = NULL;
+             *pyplot_title  = NULL,
+             *py_title_args = NULL,
+             *py_str  = NULL,
+             *title         = NULL;
 
     Py_Initialize();
+
+    PyObject *sys = PyImport_ImportModule("sys");
+    PyObject *path = PyObject_GetAttrString(sys, "path");
+    PyList_Append(path, PyString_FromString("."));
 
     pyplotname = PyString_FromString("matplotlib.pyplot");
     pyplotmodule = PyImport_Import(pyplotname);
@@ -29,16 +34,19 @@ int main()
     if (!pyplotmodule)
         return pycatch("Could not load module 'matplotlib.pyplot'.");
 
-    pyplot_figure = PyObject_GetAttrString(pyplotmodule, "figure");
-    if (!PyFunction_Check(pyplot_figure))
-        return pycatch("Could not find 'figure'.");
+    pyplot_title = PyObject_GetAttrString(pyplotmodule, "title");
+    if (!PyFunction_Check(pyplot_title))
+        return pycatch("Could not find 'title'.");
 
-    py_empty_args = PyTuple_New(0);
+    py_title_args = PyTuple_New(1);
+    py_str = PyString_FromString("My Plot");
+    PyTuple_SetItem(py_title_args, 0, py_str);
 
-    fig = PyObject_CallObject(pyplot_figure, py_empty_args);
+    title = PyObject_CallObject(pyplot_title, py_title_args);
+    Py_XDECREF(py_str);
 
-    if (!fig)
-        return pycatch("Call to figure() failed.");
+    if (!title)
+        return pycatch("Call to title() failed.");
 
     Py_Finalize();
 
