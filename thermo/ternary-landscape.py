073032f12abd4e233d3212975a256874e913e3eb
Trevor Keller <trevor.keller@nist.gov>
2019-09-25 17:02:14 -0400

    improved landscapes

--- thermo/ternary-landscape.py
+++ thermo/ternary-landscape.py
@@ -5,9 +5,7 @@
 
 from math import ceil, sqrt
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
 import numpy as np
-from scipy.optimize import fsolve
 
 from constants import *
 from pyCinterface import *
@@ -15,13 +13,21 @@ from pyCinterface import *
 pltsize = 10
 density = 1000
 ncontour = 200
-fceil = 3e9
 grScl = 21
-dark = grScl / 10
-light = grScl - 2
+dark = int(grScl / 10)
+light = int(grScl - 4)
 grays = plt.cm.gray(np.linspace(0, 1, grScl))
 alignment = {"horizontalalignment": "center", "verticalalignment": "center"}
 
+xlGCr = 0.51
+xlGNb = 0.02
+xlGNi = 1 - xlGCr - xlGNb
+xlDCr = 0.01
+xlDNb = 0.25
+xlDNi = 1 - xlDCr - xlDNb
+xlLCr = 0.35
+xlLNb = 0.27
+xlLNi = 1 - xlLCr - xlLNb
 
 def labelAxes(xlabel, ylabel, n):
     plt.axis("off")
@@ -30,14 +36,14 @@ def labelAxes(xlabel, ylabel, n):
     plt.ylim([-0.05, simY(1.05)])
 
     def plot_ticks(start, stop, tick, angle, n):
-        plt.text(0.5, -0.075, xlabel, fontsize=18, color=grays[dark])
+        plt.text(0.5, -0.075, xlabel, fontsize=18, color=grays[light])
         plt.text(
             simX(-0.11, 0.55),
             simY(0.55),
             ylabel,
             rotation=60,
             fontsize=18,
-            color=grays[dark],
+            color=grays[light],
         )
 
         # from https://stackoverflow.com/a/30975434/5377275
@@ -53,7 +59,7 @@ def labelAxes(xlabel, ylabel, n):
                     y[i] + dy,
                     "{0:.1f}".format(r[i]),
                     rotation=angle,
-                    color=grays[dark],
+                    color=grays[light],
                     **alignment
                 )
         else:
@@ -64,12 +70,12 @@ def labelAxes(xlabel, ylabel, n):
                 midy + dy,
                 "0.5",
                 rotation=angle,
-                color=grays[dark],
+                color=grays[light],
                 **alignment
             )
         x = np.vstack((x, x + tick[0]))
         y = np.vstack((y, y + tick[1]))
-        plt.plot(x, y, "k", lw=1, color=grays[dark])
+        plt.plot(x, y, "k", lw=1, color=grays[light])
 
     # Spatial considerations
     tick_size = 0.075
@@ -84,108 +90,87 @@ def labelAxes(xlabel, ylabel, n):
 
     XS = [0, simX(1, 0), simX(0, 1), 0]
     YS = [0, simY(0), simY(1), 0]
-    plt.plot(XS, YS, "-k", zorder=2, color=grays[dark])
+    plt.plot(XS, YS, "-k", zorder=2, color=grays[light])
     plot_ticks(left, right, bottom_tick, 0, n)
     plot_ticks(right, top, right_tick, -60, n)
     plot_ticks(left, top, left_tick, 60, n)
 
-
-fig = plt.figure(figsize=(pltsize, rt3by2 * pltsize))
-plt.title("Cr-Nb-Ni at {0} K".format(int(temp)), fontsize=18, color=grays[dark])
-labelAxes(r"$x_{\mathrm{Nb}}$", r"$x_{\mathrm{Cr}}$", 10)
-
-plt.text(
-    simX(0.050, 0.450),
-    simY(0.450),
-    "$\gamma$",
-    color="black",
-    alpha=0.1,
-    fontsize=20,
-    **alignment
-)
-plt.text(
-    simX(0.235, 0.050),
-    simY(0.050),
-    "$\delta$",
-    color="black",
-    alpha=0.1,
-    fontsize=20,
-    **alignment
-)
-plt.text(
-    simX(0.280, 0.365),
-    simY(0.365),
-    "$\lambda$",
-    color="black",
-    alpha=0.1,
-    fontsize=20,
-    **alignment
-)
-
 x = []
 y = []
-z = []
+zc = []
+zp = []
 
 for xNbTest in np.linspace(0, 1, density):
     for xCrTest in np.linspace(0, 1 - xNbTest, max(1, ceil((1 - xNbTest) * density))):
-        fGam = g_gam(xCrTest, xNbTest)
-        fDel = g_del(xCrTest, xNbTest)
-        fLav = g_lav(xCrTest, xNbTest)
-
-        minima = np.asarray([fGam, fDel, fLav])
-        minidx = np.argmin(minima)
-
         x.append(simX(xNbTest, xCrTest))
         y.append(simY(xCrTest))
 
-        if minidx == 0:
-            z.append(fGam)
-        elif minidx == 1:
-            z.append(fDel)
-        elif minidx == 2:
-            z.append(fLav)
+        cGam = CALPHAD_gam(xCrTest, xNbTest)
+        cDel = 0 if xNbTest > 0.25 else CALPHAD_del(xCrTest, xNbTest)
+        cLav = 0 if xNbTest > 0.3333 else CALPHAD_lav(xCrTest, xNbTest)
 
-fmin = min(z)
-fmax = max(z)
+        minima = np.asarray([cGam, cDel, cLav])
+        minidx = np.argmin(minima)
+
+        if minidx == 0:
+            zc.append(cGam)
+        elif minidx == 1:
+            zc.append(cDel)
+        elif minidx == 2:
+            zc.append(cLav)
+
+        pGam = g_gam(xCrTest, xNbTest)
+        pDel = g_del(xCrTest, xNbTest)
+        pLav = g_lav(xCrTest, xNbTest)
+
+        minima = np.asarray([pGam, pDel, pLav])
+        minidx = np.argmin(minima)
+
+        if minidx == 0:
+            zp.append(pGam)
+        elif minidx == 1:
+            zp.append(pDel)
+        elif minidx == 2:
+            zp.append(pLav)
 
 x = np.asarray(x)
 y = np.asarray(y)
-z = np.asarray(z)
 
-z[z > fceil] = fceil
+zc = np.asarray(zc)
+fcmin = min(zc)
+fcmax = max(zc)
+fcran = fcmax - fcmin
 
-levels = np.linspace(0, fceil, ncontour)
+print("CALPHAD energies span [{0:10.3e}, {1:10.3e}]; range is {2:10.3e}".format(fcmin, fcmax, fcran))
 
-plt.tricontourf(x, y, z, levels, cmap=plt.cm.get_cmap("binary"))
+zp = np.asarray(zp)
+fpmin = min(zp)
+zp[zp > fpmin + fcran] = fpmin + fcran # Danger: data manipulation!
+fpmax = max(zp)
+fpran = fpmax - fpmin
 
-npz = np.load("tie-lines.npz")
-for a, b in npz["arr_0"]:  # sAB
-    plt.scatter(a, b, color="black", alpha=0.010, s=0.5)
-for a, c in npz["arr_1"]:  # sAC
-    plt.scatter(a, c, color="black", alpha=0.010, s=0.5)
-for b, c in npz["arr_2"]:  # sBC
-    plt.scatter(b, c, color="black", alpha=0.010, s=0.5)
-for a, b in npz["arr_3"][::10]:  # sBA
-    plt.scatter(a, b, color="black", alpha=0.010, s=0.5)
-for a, c in npz["arr_4"][::10]:  # sCA
-    plt.scatter(a, c, color="black", alpha=0.010, s=0.5)
-for b, c in npz["arr_5"]:  # sCB
-    plt.scatter(b, c, color="black", alpha=0.010, s=0.5)
+print("Parabol energies span [{0:10.3e}, {1:10.3e}]; range is {2:10.3e}".format(fpmin, fpmax, fpran))
 
-npz = np.load("metastable-lines.npz")
-for a, b in npz["arr_0"]:  # mAB
-    plt.scatter(a, b, color="black", alpha=0.005, s=0.5)
-for a, c in npz["arr_1"]:  # mAC
-    plt.scatter(a, c, color="black", alpha=0.005, s=0.5)
-for b, c in npz["arr_2"]:  # mBC
-    plt.scatter(b, c, color="black", alpha=0.005, s=0.5)
-for a, b in npz["arr_3"][::10]:  # mBA
-    plt.scatter(a, b, color="black", alpha=0.005, s=0.5)
-for a, c in npz["arr_4"][::10]:  # mCA
-    plt.scatter(a, c, color="black", alpha=0.005, s=0.5)
-for b, c in npz["arr_5"]:  # mCB
-    plt.scatter(b, c, color="black", alpha=0.005, s=0.5)
 
+fig = plt.figure(figsize=(2 * pltsize, rt3by2 * pltsize))
+
+plt.subplot(1, 2, 1)
+plt.title("CALPHAD", fontsize=16, color=grays[light])
+labelAxes(r"$x_{\mathrm{Nb}}$", r"$x_{\mathrm{Cr}}$", 10)
+
+levels = np.linspace(fcmin, fcmax, ncontour)
+plt.tricontourf(x, y, zc, levels, cmap=plt.cm.get_cmap("binary"))
+
+plt.subplot(1, 2, 2)
+plt.title("Paraboloid", fontsize=16, color=grays[light])
+labelAxes(r"$x_{\mathrm{Nb}}$", r"$x_{\mathrm{Cr}}$", 10)
+
+levels = np.linspace(fpmin, fpmax, ncontour)
+plt.tricontourf(x, y, zp, levels, cmap=plt.cm.get_cmap("binary"))
+
+plt.text(-0.25, 0.95, "Cr-Nb-Ni at {0} K".format(int(temp)), fontsize=18, color=grays[light])
+
+fig.subplots_adjust(wspace=0, hspace=0)
 plt.savefig(
     "ternary-landscape.png",
     dpi=400,
@@ -193,3 +178,33 @@ plt.savefig(
     facecolor="black",
     edgecolor=None,
 )
+
+plt.close()
+
+plt.figure(figsize=(pltsize, rt3by2 * pltsize))
+plt.title("CALPHAD Cr-Nb-Ni at {0} K".format(int(temp)), fontsize=18, color=grays[light])
+labelAxes(r"$x_{\mathrm{Nb}}$", r"$x_{\mathrm{Cr}}$", 10)
+levels = np.linspace(fcmin, fcmax, ncontour)
+plt.tricontourf(x, y, zc, levels, cmap=plt.cm.get_cmap("binary"))
+plt.text(simX(xlGNb, xlGCr), simY(xlGCr), "$\gamma$",
+         color=grays[light], fontsize=16, zorder=2, **alignment)
+plt.text(xlDNb, xlDCr, "$\delta$",
+         color=grays[dark], fontsize=16, zorder=2, **alignment)
+plt.text(simX(xlLNb, xlLCr), simY(xlLCr), "$\lambda$",
+    color=grays[dark], fontsize=16, zorder=2, **alignment)
+plt.savefig("calphad-landscape.png", dpi=400, bbox_inches="tight", facecolor="black", edgecolor=None)
+plt.close()
+
+plt.figure(figsize=(pltsize, rt3by2 * pltsize))
+plt.title("Paraboloid Cr-Nb-Ni at {0} K".format(int(temp)), fontsize=18, color=grays[light])
+labelAxes(r"$x_{\mathrm{Nb}}$", r"$x_{\mathrm{Cr}}$", 10)
+levels = np.linspace(fpmin, fpmax, ncontour)
+plt.tricontourf(x, y, zp, levels, cmap=plt.cm.get_cmap("binary"))
+plt.text(simX(xlGNb, xlGCr), simY(xlGCr), "$\gamma$",
+         color=grays[dark], fontsize=16, zorder=2, **alignment)
+plt.text(xlDNb, xlDCr, "$\delta$",
+         color=grays[dark], fontsize=16, zorder=2, **alignment)
+plt.text(simX(xlLNb, xlLCr), simY(xlLCr), "$\lambda$",
+    color=grays[dark], fontsize=16, zorder=2, **alignment)
+plt.savefig("paraboloid-landscape.png", dpi=400, bbox_inches="tight", facecolor="black", edgecolor=None)
+plt.close()
