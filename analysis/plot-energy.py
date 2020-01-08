ba44294a0721ea6fc651b7a7c255274237bdcbfc
Trevor Keller <trevor.keller@nist.gov>
2020-01-08 14:09:37 -0500

    apparent limit of diffusion control

--- analysis/plot-energy.py
+++ analysis/plot-energy.py
@@ -4,23 +4,24 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-if len(sys.argv) < 2:
-    print("Usage: {0} filename".format(sys.argv[0]))
+if len(sys.argv) < 3:
+    print("Usage: {0} input output".format(sys.argv[0]))
 else:
-    filename = sys.argv[1]
+    datname = sys.argv[1]
+    imgname = sys.argv[2]
+    imgtitl = imgname.replace(".png", "")
+
     delta, lam, f = np.genfromtxt(
-        filename, skip_header=1, skip_footer=0, usecols=(4, 5, 6), unpack=True, dtype=float
+        datname, skip_header=1, skip_footer=0, usecols=(4, 5, 6), unpack=True, dtype=float
     )
 
     dt = 0.1
     t = dt * np.arange(0, len(delta))
 
-    imgname = filename.replace("log", "png")
-    if imgname == filename:
-        imgname = filename + ".png"
 
     fig, ax = plt.subplots(2, 1, sharex=True)
 
+    ax[0].set_title(imgtitl, fontsize=20)
     ax[0].set_xlabel("$t$", fontsize=20)
     ax[0].set_ylabel("$\phi$", rotation = 0, fontsize=20, labelpad=20)
     ax[0].plot(t, delta, label="$\delta$")
