8d8d20bd9b0fcb1d4f7413aa5b323877b873ec4b
Trevor Keller <trevor.keller@nist.gov>
2020-11-16 18:27:36 -0500

    improved IC, fixed fn calls

--- fipy/precipitate-aging.py
+++ fipy/precipitate-aging.py
@@ -12,9 +12,14 @@ from energies import *
 Lx = 1e-6
 dx = 5e-9
 Nx = Lx / dx
+# Nx = 3
 
+# System composition
+f0 = 0.125
 xCr0 = 0.3125
 xNb0 = 0.1575
+xCrGam0 = (1 - f0) * xCr0 - f0 / (1 - f0) * xe_del_Cr
+xNbGam0 = (1 - f0) * xNb0 - f0 / (1 - f0) * xe_del_Nb
 
 DCrCr =  3.96e-18
 DCrNb = -3.40e-17
@@ -35,51 +40,52 @@ def pf_tanh(x, h):
 mesh = fp.Grid1D(dx=dx, nx=Nx)
 x = mesh.cellCenters[0]
 
-phiD = fp.CellVariable(name="$\delta$", mesh=mesh, value=pf_tanh(x, Lx / 8))
-phiG = fp.CellVariable(name="$\gamma$", mesh=mesh, value=1 - p(phiD))
-phiL = fp.CellVariable(name="$\lambda$", mesh=mesh, value=0.)
+phiD = fp.CellVariable(name="$\phi^{\delta}$", mesh=mesh, value=pf_tanh(x, Lx / 8))
+phiL = fp.CellVariable(name="$\phi^{\lambda}$", mesh=mesh, value=0.)
+pD = p(phiD)
+pL = p(phiD)
+pG = fp.CellVariable(name="$p^{\gamma}$", mesh=mesh, value=1 - pD - pL)
 
-xCr = fp.CellVariable(name=r"$x_{\mathrm{Cr}}$", mesh=mesh, value=xe_gam_Cr * phiG + xe_del_Cr * phiD)
-xNb = fp.CellVariable(name=r"$x_{\mathrm{Nb}}$", mesh=mesh, value=xe_gam_Nb * phiG + xe_del_Nb * phiD)
-xNi = fp.CellVariable(name=r"$x_{\mathrm{Ni}}$", mesh=mesh, value=1 - xCr - xNb)
+xCr = fp.CellVariable(name=r"$x_{\mathrm{Cr}}$", mesh=mesh, value=xCrGam0 * pG + xe_del_Cr * pD + xe_lav_Cr * pL)
+xNb = fp.CellVariable(name=r"$x_{\mathrm{Nb}}$", mesh=mesh, value=xNbGam0 * pG + xe_del_Nb * pD + xe_lav_Nb * pL)
 
-xCrGam = fp.CellVariable(name=r"$x^{\gamma}_{\mathrm{Cr}}$", mesh=mesh, value=x_gam_Cr(xCr, xNb, phiD, phiG, phiL))
-xNbGam = fp.CellVariable(name=r"$x^{\gamma}_{\mathrm{Nb}}$", mesh=mesh, value=x_gam_Nb(xCr, xNb, phiD, phiG, phiL))
-xCrDel = fp.CellVariable(name=r"$x^{\delta}_{\mathrm{Cr}}$", mesh=mesh, value=x_del_Cr(xCr, xNb, phiD, phiG, phiL))
-xNbDel = fp.CellVariable(name=r"$x^{\delta}_{\mathrm{Nb}}$", mesh=mesh, value=x_del_Nb(xCr, xNb, phiD, phiG, phiL))
+xCrGam = fp.CellVariable(name=r"$x^{\gamma}_{\mathrm{Cr}}$", mesh=mesh, value=x_gam_Cr(xCr, xNb, pD, pG, pL))
+xNbGam = fp.CellVariable(name=r"$x^{\gamma}_{\mathrm{Nb}}$", mesh=mesh, value=x_gam_Nb(xCr, xNb, pD, pG, pL))
+xCrDel = fp.CellVariable(name=r"$x^{\delta}_{\mathrm{Cr}}$", mesh=mesh, value=x_del_Cr(xCr, xNb, pD, pG, pL))
+xNbDel = fp.CellVariable(name=r"$x^{\delta}_{\mathrm{Nb}}$", mesh=mesh, value=x_del_Nb(xCr, xNb, pD, pG, pL))
 
 # Equation numbers refer to the draft manuscript.
 
-eq12a = fp.ImplicitSourceTerm(coeff=phiG, var=xCrGam) \
-      + fp.ImplicitSourceTerm(coeff=phiD, var=xCrDel) \
-      == fp.ImplicitSourceTerm(coeff=1, var=xCr)
+eq12a = fp.ImplicitSourceTerm(coeff=pG, var=xCrGam) \
+      + fp.ImplicitSourceTerm(coeff=pD, var=xCrDel) \
+     == fp.ImplicitSourceTerm(coeff=1, var=xCr)
 
-eq12b = fp.ImplicitSourceTerm(coeff=phiG, var=xNbGam) \
-      + fp.ImplicitSourceTerm(coeff=phiD, var=xNbDel) \
-      == fp.ImplicitSourceTerm(coeff=1, var=xNb)
+eq12b = fp.ImplicitSourceTerm(coeff=pG, var=xNbGam) \
+      + fp.ImplicitSourceTerm(coeff=pD, var=xNbDel) \
+     == fp.ImplicitSourceTerm(coeff=1, var=xNb)
 
-eq12c  = fp.ImplicitSourceTerm(coeff=0.5*d2g_gam_dxCrCr(), var=xCrGam) \
-       + fp.ImplicitSourceTerm(coeff=d2g_gam_dxCrNb(), var=xNbGam) \
-       - fp.ImplicitSourceTerm(coeff=0.5*d2g_del_dxCrCr(), var=xCrDel) \
-       - fp.ImplicitSourceTerm(coeff=d2g_del_dxCrNb(), var=xNbDel) \
-      == 0.5 * xe_gam_Cr * d2g_gam_dxCrCr() + xe_gam_Nb * d2g_gam_dxCrNb() \
-       - 0.5 * xe_del_Cr * d2g_del_dxCrCr() - xe_del_Nb * d2g_del_dxCrNb()
+eq12c = fp.ImplicitSourceTerm(coeff=0.5*d2g_gam_dxCrCr(), var=xCrGam) \
+      + fp.ImplicitSourceTerm(coeff=d2g_gam_dxCrNb(), var=xNbGam) \
+      - fp.ImplicitSourceTerm(coeff=0.5*d2g_del_dxCrCr(), var=xCrDel) \
+      - fp.ImplicitSourceTerm(coeff=d2g_del_dxCrNb(), var=xNbDel) \
+     == 0.5 * xe_gam_Cr * d2g_gam_dxCrCr() + xe_gam_Nb * d2g_gam_dxCrNb() \
+      - 0.5 * xe_del_Cr * d2g_del_dxCrCr() - xe_del_Nb * d2g_del_dxCrNb()
 
-eq12d  = fp.ImplicitSourceTerm(coeff=d2g_gam_dxCrNb(), var=xCrGam) \
-       + fp.ImplicitSourceTerm(coeff=0.5*d2g_gam_dxNbNb(), var=xNbGam) \
-       - fp.ImplicitSourceTerm(coeff=d2g_del_dxCrNb(), var=xCrDel) \
-       - fp.ImplicitSourceTerm(coeff=0.5*d2g_del_dxNbNb(), var=xNbDel) \
-      == xe_gam_Cr * d2g_gam_dxCrNb() + 0.5 * xe_gam_Nb * d2g_gam_dxNbNb() \
-       - xe_del_Cr * d2g_del_dxCrNb() - 0.5 * xe_del_Nb * d2g_del_dxNbNb()
+eq12d = fp.ImplicitSourceTerm(coeff=d2g_gam_dxCrNb(), var=xCrGam) \
+      + fp.ImplicitSourceTerm(coeff=0.5*d2g_gam_dxNbNb(), var=xNbGam) \
+      - fp.ImplicitSourceTerm(coeff=d2g_del_dxCrNb(), var=xCrDel) \
+      - fp.ImplicitSourceTerm(coeff=0.5*d2g_del_dxNbNb(), var=xNbDel) \
+     == xe_gam_Cr * d2g_gam_dxCrNb() + 0.5 * xe_gam_Nb * d2g_gam_dxNbNb() \
+      - xe_del_Cr * d2g_del_dxCrNb() - 0.5 * xe_del_Nb * d2g_del_dxNbNb()
 
-pressure = p_prime(phiD) * g_gamma(xCrGam, xNbGam) \
-         - p_prime(phiD) * g_delta(xCrGam, xNbGam) \
-         - fp.ImplicitSourceTerm(var=xCrGam, coeff=p_prime(phiD) * dg_gam_dxCr(xCr, xNb)) \
-         + fp.ImplicitSourceTerm(var=xCrDel, coeff=p_prime(phiD) * dg_gam_dxCr(xCr, xNb)) \
-         - fp.ImplicitSourceTerm(var=xNbGam, coeff=p_prime(phiD) * dg_gam_dxNb(xCr, xNb)) \
-         + fp.ImplicitSourceTerm(var=xNbDel, coeff=p_prime(phiD) * dg_gam_dxNb(xCr, xNb))
+pressure = p_prime(phiD) * (g_gamma(xCrGam, xNbGam) - g_delta(xCrDel, xNbDel)) \
+         - fp.ImplicitSourceTerm(var=xCrGam, coeff=p_prime(phiD) * dg_gam_dxCr(xCrGam, xNbGam)) \
+         + fp.ImplicitSourceTerm(var=xCrDel, coeff=p_prime(phiD) * dg_gam_dxCr(xCrGam, xNbGam)) \
+         - fp.ImplicitSourceTerm(var=xNbGam, coeff=p_prime(phiD) * dg_gam_dxNb(xCrGam, xNbGam)) \
+         + fp.ImplicitSourceTerm(var=xNbDel, coeff=p_prime(phiD) * dg_gam_dxNb(xCrGam, xNbGam))
 
-eq24 = fp.TransientTerm(coeff=1.0/Ldel, var=phiD) == pressure \
+eq24 = fp.TransientTerm(coeff=1.0/Ldel, var=phiD) \
+    == pressure \
      - 2 * omega * phiD * (1 - phiD) * (1 - 2 * phiD) \
      + fp.DiffusionTerm(coeff=kappa, var=phiD)
 
@@ -93,22 +99,27 @@ eq27b = fp.TransientTerm(var=xNb) == fp.DiffusionTerm(coeff=DNbCr, var=xCrGam) \
                                    + fp.DiffusionTerm(coeff=DNbCr, var=xCrDel) \
                                    + fp.DiffusionTerm(coeff=DNbNb, var=xNbDel)
 
-eq = eq12a & eq12b & eq12c & eq12d & eq24 & eq27a & eq27b
+coupled = eq12a & eq12b & eq12c & eq12d & eq24 & eq27a & eq27b
 
 # Turn the crank
 
-viewer = fp.Viewer(vars=(phiD, xCr, xNb), datamin=0, datamax=1)
+viewer = fp.Viewer(vars=(phiD, xCr, xNb))
 viewer.plot()
 
-dt = 1.0e-9
-t = fp.Variable(name="$t$", value=0.)
-i = 0
+fp.input("Initial condition. Press <return> to proceed...")
 
-while (t.value < 1):
+dt = 1.0e-8
+t = fp.Variable(name="$t$", value=0.)
+
+for i in range(10):
+    print("t =", t.value, "sec")
     # eq.solve(dt=dt)
     for sweep in range(5):
-        eq.sweep(dt=dt)
+        res = coupled.sweep(dt=dt)
+        print("    ", res)
     t.value = t() + dt
-    i += 1
-    if (i % 1000):
-        viewer.plot()
+    # i = i + 1
+    # if (i % 1000):
+    #     viewer.plot()
+
+viewer.plot()
